e4b12c6da044b0e37751fb609c62aed634122e62
wang1128 <penghao@Penghao-Macbook-Pro.local>
2016-12-30 10:46:08 -0500

    1230

--- sessionBasic.py
+++ sessionBasic.py
@@ -1,2 +1,20 @@
 #mofan on youtube 1229
 
+import tensorflow as tf
+
+matrix1 = tf.constant([[3,3]])
+matrix2 = tf.constant([[2],[2]])
+
+product = tf.matmul(matrix1,matrix2)  #matrix multiply np.dot(m1,m2)
+
+# # method 1
+# sess = tf.Session()
+# result = sess.run(product)
+# print(result)
+# sess.close()
+
+# method 2
+with tf.Session() as sess:  #注意 Session() 大写 和 括号
+    result2 = sess.run(product)
+    print(result2)
+    
