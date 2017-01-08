96d1cdd284a7c3654d2cd88f1a2dd0155f474643
wang1128 <penghao@Penghao-Macbook-Pro.local>
2017-01-07 20:27:48 -0500

    1.7

--- addLayer.py
+++ addLayer.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
+import numpy as np
 #激励函数 以后可以再详细看下 掰弯的函数
 
 #google tensorflow af
 
 def add_layer(inputs,in_size,out_size,activation_function = None ):
     Weights = tf.Variable(tf.random_normal([in_size,out_size]))
-    biases = tf.Variable(tf.zeros(1,out_size) + 0.1)# 1 行 out_size 列
+    biases = tf.Variable(tf.zeros([1,out_size]) + 0.1)# 1 行 out_size 列
     Wx_plus_b = tf.matmul(inputs,Weights) + biases
 
     if activation_function is None:
@@ -14,3 +15,31 @@ def add_layer(inputs,in_size,out_size,activation_function = None ):
         outputs = activation_function(Wx_plus_b)
 
     return outputs
+
+
+x_data = np.linspace(-1,1,300)[:,np.newaxis] #300行 300个例子
+noise = np.random.normal(0,0.05,x_data.shape) #mean 0, std 0.05, shape 和 x_data 一样
+y_data = np.square(x_data) - 0.5 + noise
+# 1, 10, 1 的网络\
+xs = tf.placeholder(tf.float32,[None,1])
+ys = tf.placeholder(tf.float32, [None,1])
+l1 = add_layer(xs,1,10,activation_function = tf.nn.relu)
+
+prediction = add_layer(l1,10,1,activation_function = None)
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices= [1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss = loss)
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    sess.run(train_step,feed_dict={xs: x_data, ys: y_data})
+    if i% 50 == 0:
+        print(sess.run(loss,feed_dict={xs: x_data, ys: y_data}))
+
+
+
+
