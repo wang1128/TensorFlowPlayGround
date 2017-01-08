d47c4471470c2ae92beaf24698efc320cec0edf7
wang1128 <penghao@Penghao-Macbook-Pro.local>
2017-01-07 20:52:20 -0500

    1.7
    almost waste my day 555

--- addLayer.py
+++ addLayer.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 import numpy as np
+#import matplotlib as mil
+#mil.use('TkAgg')
+import matplotlib.pyplot as plt
+
 #激励函数 以后可以再详细看下 掰弯的函数
 
 #google tensorflow af
@@ -35,10 +39,25 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(x_data,y_data)
+plt.ion()
+plt.show()
+
+
 for i in range(1000):
     sess.run(train_step,feed_dict={xs: x_data, ys: y_data})
     if i% 50 == 0:
-        print(sess.run(loss,feed_dict={xs: x_data, ys: y_data}))
+        #print(sess.run(loss,feed_dict={xs: x_data, ys: y_data}))
+        try:
+            ax.lines.remove(lines[0])
+        except:
+            pass
+        prediction_value = sess.run(prediction,feed_dict={xs:x_data})
+        lines = ax.plot(x_data,prediction_value,'r-',lw =5)
+
+        plt.pause(0.1)
 
 
 
